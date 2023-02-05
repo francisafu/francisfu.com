4b9f6da2c62dd090ac26cf2ed20546e274d91b80
francisfu <francis_afu@163.com>
2023-02-05 19:11:59 +0800

    Update Shell

--- static/nodes/GetNodes.sh
+++ static/nodes/GetNodes.sh
@@ -2,22 +2,22 @@
 ###
  # @Author: Francis Fu
  # @Date: 2023-02-05 15:17:06
- # @LastEditTime: 2023-02-05 19:04:28
+ # @LastEditTime: 2023-02-05 19:10:49
  # @LastEditors: Francis Fu
 ### 
 echo ""
 # 输出当前时间
 date --date='0 days ago' "+%Y-%m-%d %H:%M:%S"
-echo "-------清理目录-------"
+echo "-----清理目录-----"
 sudo rm -rf /www/wwwroot/francisfu.com/nodes/nodes
 sudo mkdir /www/wwwroot/francisfu.com/nodes/nodes
 cd /www/wwwroot/francisfu.com/nodes/nodes
 echo "-----开始下载-----"
-sudo wget -O G1B.txt https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity
-sudo wget -O G1M.txt https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity.txt
-sudo wget -O G1C.txt https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity.yml
-sudo wget -O G2B.txt https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/EternityAir
-sudo wget -O G2M.txt https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/EternityAir.txt
-sudo wget -O G2C.txt https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/EternityAir.yml
+sudo wget -q -O G1B.txt https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity
+sudo wget -q -O G1M.txt https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity.txt
+sudo wget -q -O G1C.txt https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity.yml
+sudo wget -q -O G2B.txt https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/EternityAir
+sudo wget -q -O G2M.txt https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/EternityAir.txt
+sudo wget -q -O G2C.txt https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/EternityAir.yml
 echo "-----下载成功-----"
 exit
